d91b2a1e5b60d9ef1210bcf28953ef290996da74
DESKTOP-EJPIOLD\dev_byj <myosikb@gmail.com>
2022-04-12 15:23:34 +0900

    useCallback

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,17 +1,32 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import AppLayout from "../components/AppLayout";
 import Head from 'next/head';
 import { Form, Input, Button, Checkbox } from 'antd';
 
 export default function Signup() {
-    const [nick, setNick] = useState('');
-    const [password, setPassword] = useState('');
     const [passwordCheck, setPasswordCheck] = useState('');
     const [term, setTerm] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
     const [termError, setTermError] = useState(false);
 
-    const onFinish = (values) => {
+    const useInput = (initValue = null) => {
+        const [value, setter] = useState(initValue);
+        const handler = useCallback((e) => {
+            setter(e.target.value);
+        }, []);
+        return [value, handler];
+    }
+
+    const [id, onChangeId] = useInput('');
+    const [nick, onChangeNick] = useInput('');
+    const [password, onChangePassword] = useInput('');
+    
+    // useCallback을 사용해서 event Listener들을 감싸주기
+    // eventListener들이 다 antd 컴포넌트 안에 들어가는데 props로 넘겨주는 함수, 메소드들은 useCallback으로 감싸주어야 함
+    // 함수 컴포넌트가 state가 바뀔 때마다 전체가 재 실행되면서 함수들이 새로 생성됨
+    // 그 함수를 전달받은 자식 컴포넌트 들은 렌더링을 다시 한다는 뜻 함수도 객체이기 때문에 객체는 새로 생성하게 되면 이전 객체와는 다른 객체가 됨
+    // 그럼 의도치 않게 리 렌더링이 발생하기 때문에 useCallback으로 감싸주는 것이 좋음 
+    const onFinish = useCallback((values) => {
         e.preventDefault();
         console.log('Received values of form: ', values);
         if(password !== passwordCheck) {
@@ -20,28 +35,20 @@ export default function Signup() {
         if(!term) {
             return setTermError(true);
         }
-    };
-    const onChangeNick = (e) => { setNick(e.target.value); console.log("onChangeNick"); console.log(e.target.value); };
-    const onChangePassword = (e) => { setPassword(e.target.value) };
-    
-    const onChangePasswordCheck = (e) => {
+    }, [password, passwordCheck, term]);
+    // state들도 dependency array에 담아주어야 함
+
+    const onChangePasswordCheck = useCallback((e) => {
         setPasswordError(e.target.value !== password);
         setPasswordCheck(e.target.value); 
-    };
-    const onChangeTerm = (e) => { 
+    }, [password]);
+
+    const onChangeTerm = useCallback((e) => { 
         setTermError(false);
         setTerm(e.target.checked); 
-    };
+    }, []);
 
-    const useInput = (initValue = null) => {
-        const [value, setter] = useState(initValue);
-        const handler = (e) => {
-            setter(e.target.value);
-        }
-        return [value, handler];
-    }
 
-    const [id, onChangeId] = useInput('');
 
     return (
         <>
