f4b93211a1b0687b90ea8906f767db1728530d81
DESKTOP-EJPIOLD\dev_byj <myosikb@gmail.com>
2022-04-13 15:17:47 +0900

    antd 그리드 시스템

--- frontend/components/AppLayout.js
+++ frontend/components/AppLayout.js
@@ -1,8 +1,14 @@
-import { Menu, Input, Button } from 'antd';
+import { Menu, Input, Button, Row, Col, Card, Avatar } from 'antd';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 
 const { Search } = Input;
+const dummy = {
+    nickname: '제로초',
+    Post: [],
+    Followings: [],
+    Followers: []
+}
 
 export default function AppLayout({children}) {
     return (
@@ -15,6 +21,26 @@ export default function AppLayout({children}) {
                 </Menu.Item>
             </Menu>
             <Link href="/signup"><a><Button>회원가입</Button></a></Link>
+            <Row>
+                <Col xs={24} md={6}>
+                    <Card
+                        actions={[
+                            <div key="twit">짹짹<br />{dummy.Post.length}</div>,
+                            <div key="following">팔로잉<br />{dummy.Followings.length}</div>,
+                            <div key="follower">팔로워<br />{dummy.Followers.length}</div>,
+                        ]}
+                    >
+                        <Card.Meta
+                            avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
+                            title={dummy.nickname}
+                        />
+                    </Card>
+                </Col>
+                <Col xs={24} md={12}>
+                    {children}
+                </Col>
+                <Col xs={24} md={6}>세번째</Col>
+            </Row>
             {children}
         </>
     )
